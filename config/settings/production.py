94cd85877c7b3a26a1037540b22e17f15de942c7
oluwakayode-a <khaox07@gmail.com>
2021-07-21 11:26:17 +0100

    Heroku setup

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 import os
+import dj_database_url
 
 my_debug = os.getenv('DEBUG')
 if my_debug == "False":
@@ -8,8 +9,10 @@ else:
     DEBUG = True
 
 
+ALLOWED_HOSTS = ['cowrywise-backend.herokuapp.com']
+
 SECRET_KEY = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
